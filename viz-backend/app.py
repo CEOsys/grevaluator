61a8a8b20d0938e7d5653e0d545704b87629197c
Gregor Lichtner <gregor.lichtner@charite.de>
2021-03-24 21:10:25 +0100

    initial viz backend

---
+++ viz-backend/app.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+import pandas as pd
+
+app = FastAPI()
+
+data = {}
+
+
+@app.on_event("startup")
+async def startup_event():
+    data["patients"] = pd.read_csv("../data/sample_data_shuffle.csv.gz")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Patient Viz Server"}
+
+
+@app.get("/patient/list/")
+async def list_patients():
+    ret = data["patients"]["pseudo_fallnr"].unique()
+    return list(ret)
+
+
+@app.get("/patient/get/{fallnr}")
+async def list_patient(fallnr: str):
+    patient = data["patients"].query("pseudo_fallnr==@fallnr")
+
+    if len(patient) == 0:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    return patient.fillna("")
