b77515301a234375a85cbb2ba21f0ec17d32eee7
Gregor Lichtner <gregor.lichtner@charite.de>
2021-04-16 10:20:12 +0200

    licensed under AGPL

--- apps/viz-backend/main.py
+++ apps/viz-backend/main.py
@@ -1,3 +1,20 @@
+#  This file is part of CEOsys Recommendation Checker.
+#
+#  Copyright (c) 2021 CEOsys project team <https://covid-evidenz.de>.
+#
+#  CEOsys Recommendation Checker is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  CEOsys Recommendation Checker is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with CEOsys Recommendation Checker.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 from pathlib import Path
 from datetime import datetime, timedelta
